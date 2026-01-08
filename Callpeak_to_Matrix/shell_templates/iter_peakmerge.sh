51b7218ecaa663c38980ab4cea6d2902fa9503d9
Ryan <rlancione@health.ucsd.edu>
2026-01-08 00:58:25 -0800

    callpeak, mergepeak functionality added

---
+++ Callpeak_to_Matrix/shell_templates/iter_peakmerge.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+GENOME="hg38"
+
+BL="/tscc/projects/ps-epigen/GENOME/hg38/hg38.blacklist.bed.gz"
+#BL="/tscc/projects/ps-epigen/GENOME/mm10/mm10.blacklist.bed.gz"
+
+CS="/tscc/projects/ps-epigen/GENOME/hg38/hg38.chrom.sizes"
+#CS="/tscc/projects/ps-epigen/GENOME/mm10/mm10.chrom.sizes"
+
+OUT='[OUTDIR]'
+
+NAME='[NAME]'
+
+Rscript /tscc/projects/ps-epigen/bpToolbox/snATAC/iterative_overlap_peak_merging.R -i my_summits.txt \
+        -g $GENOME\
+        --blacklist $BL\
+        --chromSize $CS \
+        -d $OUT -o $NAME
---
+++ Callpeak_to_Matrix/shell_templates/pairtag_peakmerge.sh
@@ -0,0 +1,22 @@
+### To bedtools merge
+# 1. concat bed files
+# 2. sort bed filed
+# 3. bedtools merge 
+
+
+IDIR='[IPEAKS]'
+ODIR='[ODIR]'
+NAME='[NAME]'
+
+mkdir -p $ODIR
+
+FILES=$(find "$IDIR" -type f -name "*narrow*" -readable)
+echo $FILES
+
+
+cat $FILES > ${ODIR}${NAME}_concat.bed
+sort -k1,1 -k2,2n ${ODIR}${NAME}_concat.bed > ${ODIR}${NAME}_concat_sorted.bed
+
+bedtools merge -i ${ODIR}${NAME}_concat_sorted.bed > ${ODIR}${NAME}_merged.bed
+
+
